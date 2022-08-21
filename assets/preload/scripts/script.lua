10dfd5e6cf1d2782922a10f9d03239941ad3f509
TessLMAO <110305665+TessLMAO@users.noreply.github.com>
2022-08-21 19:25:42 -0300

    Add files via upload

---
+++ assets/preload/scripts/script.lua
@@ -0,0 +1,6 @@
+function onCreate()
+	setPropertyFromClass('GameoverSubtate', 'characterName', 'Amy');
+	setPropertyFromClass('GameoverSubstate', 'deathSoundName', 'fnf_loss_sfx');
+	setPropertyFromClass('GameoverSubstate', 'loopSoundName', 'gameOver');
+	setPropertyFromClass('GameoverSubstate', 'endSoundName', 'what-are-friends-for');
+end 
